197a90eff2451a5dda8eef9ba7ccad807f14a406
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-14 18:08:03 -0300

    chore: update testing setup with vitest and add initial test case
    - Updated package.json to include new test scripts and vitest dependencies.
    - Added a new test file for basic vitest functionality.
    - Created a vitest configuration file to support tsconfig paths.

---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe('Check vitest is working', () => {
+  it('should be able to run vitest', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
