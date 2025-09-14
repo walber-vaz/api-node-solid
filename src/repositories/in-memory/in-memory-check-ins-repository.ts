476de8a8074e02d46619ae2dd839a7a425561dc5
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-14 20:07:52 -0300

    feat: implement InMemoryCheckInsRepository for check-in management

---
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -0,0 +1,21 @@
+import { randomUUID } from 'node:crypto';
+import type { CheckIn, Prisma } from '../../../generated/prisma';
+import type { iCheckInsRepository } from '../check-ins-repository';
+
+export class InMemoryCheckInsRepository implements iCheckInsRepository {
+  public items: CheckIn[] = [];
+
+  async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
+    const checkIn = {
+      id: randomUUID(),
+      gym_id: data.gym_id,
+      user_id: data.user_id,
+      created_at: new Date(),
+      validated_at: data.validated_at ? new Date(data.validated_at) : null,
+    };
+
+    this.items.push(checkIn);
+
+    return checkIn;
+  }
+}
