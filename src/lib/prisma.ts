ec0ebed53d0f2e10e7387bf1c700962a47202ed7
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-14 16:17:39 -0300

    feat: enhance PrismaClient initialization with logging for development environment

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,6 +3,10 @@ import { PrismaClient } from '../../generated/prisma';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: env.NODE_ENV === 'development' ? ['query'] : [],
+  });
 
 if (env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
