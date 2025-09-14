4d81d74e8da0df2659679f1c4c12ff8286be1ce4
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-14 15:41:11 -0300

    feat: add Prisma schema and environment configuration for PostgreSQL

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,6 +4,7 @@ import { z } from 'zod';
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
   PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.url(),
 });
 
 const _env = envSchema.safeParse(process.env);
---
+++ src/lib/prisma.ts
@@ -0,0 +1,8 @@
+import { env } from '@/config/env';
+import { PrismaClient } from '../../generated/prisma';
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
