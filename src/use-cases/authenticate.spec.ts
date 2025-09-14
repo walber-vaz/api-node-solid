552c66c58b926eff080613c352aabeb03efb6738
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-14 19:26:41 -0300

    test: add unit tests for AuthenticateUseCase to verify user authentication behavior

---
+++ src/use-cases/authenticate.spec.ts
@@ -0,0 +1,55 @@
+import { describe, expect, it } from 'vitest';
+import { hashPassword } from '@/lib/hash-password';
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-repository';
+import { AuthenticateUseCase } from './authenticate';
+import { InvalidCredentialError } from './erros/invalid-credential-error';
+
+describe('Authenticate Use Case', () => {
+  it('should be able to authenticate a user with valid credentials', async () => {
+    const userRepository = new InMemoryUsersRepository();
+    const sut = new AuthenticateUseCase(userRepository);
+
+    await userRepository.create({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password_hash: await hashPassword('securePassword123'),
+    });
+
+    const { user } = await sut.execute({
+      email: 'john.doe@example.com',
+      password: 'securePassword123',
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+  });
+
+  it('should not be able to authenticate with wrong email', async () => {
+    const userRepository = new InMemoryUsersRepository();
+    const sut = new AuthenticateUseCase(userRepository);
+
+    await expect(
+      sut.execute({
+        email: 'wrong.email@example.com',
+        password: 'securePassword123',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialError);
+  });
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const userRepository = new InMemoryUsersRepository();
+    const sut = new AuthenticateUseCase(userRepository);
+
+    await userRepository.create({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password_hash: await hashPassword('securePassword123'),
+    });
+
+    await expect(
+      sut.execute({
+        email: 'john.doe@example.com',
+        password: 'wrongPassword',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialError);
+  });
+});
