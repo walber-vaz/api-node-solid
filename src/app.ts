205eac1f9ed6951d48b9a96a73a8a2ee2ff05047
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-14 17:58:23 -0300

    fix: update error handling to include ZodError validation response

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify';
+import { ZodError, z } from 'zod';
+import { env } from '@/config/env';
 import { appRoutes } from '@/http/routes';
 
 export const app = fastify({
-  logger: {
+  logger: env.NODE_ENV !== 'test' && {
     transport: {
       target: 'pino-pretty',
       options: {
@@ -14,3 +16,22 @@ export const app = fastify({
 });
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: z.treeifyError(error),
+    });
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error);
+  } else {
+    // todo: here we should log the error to an external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error',
+  });
+});
