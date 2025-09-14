e36b281d2e42baed7546341509a10d30b8efa7ca
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-14 17:41:21 -0300

    feat: add PrismaUsersRepository implementation for user creation and retrieval

---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,21 @@
+import { prisma } from '@/lib/prisma';
+import type { Prisma } from '../../../generated/prisma';
+import type { iUsersRepository } from '../users-repository';
+
+export class PrismaUsersRepository implements iUsersRepository {
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    });
+
+    return user;
+  }
+
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    return user;
+  }
+}
