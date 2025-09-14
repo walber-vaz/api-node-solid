0e0b478dfd3c19a5003ddce22e8107a4ec81a61d
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-14 19:26:50 -0300

    feat: add session route to authenticate users

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from 'fastify';
+import { authenticate } from '@/http/controllers/authenticate';
 import { register } from '@/http/controllers/register';
 
 export const appRoutes = async (app: FastifyInstance) => {
   app.post('/users', register);
+  app.post('/sessions', authenticate);
 };
