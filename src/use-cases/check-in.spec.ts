714abfc3243832a531a80beed2a2d310706ff77e
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-14 20:08:04 -0300

    feat: add unit tests for CheckInUseCase to validate check-in creation

---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,22 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository';
+import { CheckInUseCase } from './check-in';
+
+let checkInRepository: InMemoryCheckInsRepository;
+let sut: CheckInUseCase;
+
+describe('Check-In Use Case', () => {
+  beforeEach(() => {
+    checkInRepository = new InMemoryCheckInsRepository();
+    sut = new CheckInUseCase(checkInRepository);
+  });
+
+  it('should be able to create a new check in', async () => {
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    });
+
+    expect(checkIn.id).toEqual(expect.any(String));
+  });
+});
