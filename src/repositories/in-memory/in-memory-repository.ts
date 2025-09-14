4f1d71ee2562236ee5a7c4c07c8a142771f9e0e5
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-14 18:40:41 -0300

    feat: implement in-memory user repository with create and findByEmail methods

---
+++ src/repositories/in-memory/in-memory-repository.ts
@@ -0,0 +1,30 @@
+import type { Prisma, User } from '../../../generated/prisma';
+import type { iUsersRepository } from '../users-repository';
+
+export class InMemoryUsersRepository implements iUsersRepository {
+  public items: User[] = [];
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: 'generated-id',
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    };
+
+    this.items.push(user);
+
+    return user;
+  }
+
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email);
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
+}
