d81e290edb5f30d5d3ac35227344053cc9735998
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-14 16:17:42 -0300

    feat: implement user creation endpoint with password hashing and validation

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import fastify from 'fastify';
+import { z } from 'zod';
+import { hashPassword } from '@/lib/hash-password';
+import { prisma } from '@/lib/prisma';
 
 export const app = fastify({
   logger: {
@@ -12,6 +15,23 @@ export const app = fastify({
   },
 });
 
-app.get('/', async (_request, reply) => {
-  return reply.send({ hello: 'world' });
+app.post('/users', async (request, reply) => {
+  const createUserBodySchema = z.object({
+    name: z.string(),
+    email: z.email(),
+    password: z.string().min(8),
+  });
+
+  const { email, name, password } = createUserBodySchema.parse(request.body);
+
+  const hashedPassword = await hashPassword(password);
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: hashedPassword,
+    },
+  });
+
+  return reply.status(201).send({ message: 'User created successfully' });
 });
