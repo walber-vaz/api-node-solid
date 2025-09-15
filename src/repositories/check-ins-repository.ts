dc8bb51df9b137ada7e82769aa78038d6e89db15
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-14 23:03:39 -0300

    feat: restore findManyByUserId method in iCheckInsRepository interface

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,7 +2,7 @@ import type { CheckIn, Prisma } from '@prisma/client';
 
 export interface iCheckInsRepository {
   create: (data: Prisma.CheckInUncheckedCreateInput) => Promise<CheckIn>;
+  findManyByUserId: (userId: string, page: number) => Promise<CheckIn[]>;
   findByUserIdOnDate: (userId: string, date: Date) => Promise<CheckIn | null>;
   // findById: (id: string) => Promise<CheckIn | null>;
-  // findManyByUserId: (userId: string) => Promise<CheckIn[]>;
 }
