19d9b2c6afde9ef9e12bd322cef0f9fe73989b18
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-14 23:11:56 -0300

    feat: add countByUserId method to iCheckInsRepository interface

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -4,5 +4,5 @@ export interface iCheckInsRepository {
   create: (data: Prisma.CheckInUncheckedCreateInput) => Promise<CheckIn>;
   findManyByUserId: (userId: string, page: number) => Promise<CheckIn[]>;
   findByUserIdOnDate: (userId: string, date: Date) => Promise<CheckIn | null>;
-  // findById: (id: string) => Promise<CheckIn | null>;
+  countByUserId: (userId: string) => Promise<number>;
 }
