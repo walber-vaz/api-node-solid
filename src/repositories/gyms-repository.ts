31e237904be6c1f55fff8cf545e7ac73ad6be01d
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-15 08:29:06 -0300

    feat: add findManyNearby method to iGymsRepository for nearby gym search functionality

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,7 +1,13 @@
 import type { Gym, Prisma } from '@prisma/client';
 
+export interface iFindManyNearbyParams {
+  latitude: number;
+  longitude: number;
+}
+
 export interface iGymsRepository {
   findById: (id: string) => Promise<Gym | null>;
   searchMany: (query: string, page: number) => Promise<Gym[]>;
   create: (data: Prisma.GymCreateInput) => Promise<Gym>;
+  findManyNearby: (params: iFindManyNearbyParams) => Promise<Gym[]>;
 }
