904137daf2819504fb768add55d7d501d4193395
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-15 08:15:34 -0300

    feat: add searchMany method to iGymsRepository for gym search functionality

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -2,5 +2,6 @@ import type { Gym, Prisma } from '@prisma/client';
 
 export interface iGymsRepository {
   findById: (id: string) => Promise<Gym | null>;
+  searchMany: (query: string, page: number) => Promise<Gym[]>;
   create: (data: Prisma.GymCreateInput) => Promise<Gym>;
 }
