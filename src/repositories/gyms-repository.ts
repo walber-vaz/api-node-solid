2cf4ce8d88713e447206691ded7e63a9b486cad0
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-14 22:36:41 -0300

    refactor: add Prisma import to enhance type definitions in iGymsRepository

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,5 +1,6 @@
-import type { Gym } from '@prisma/client';
+import type { Gym, Prisma } from '@prisma/client';
 
 export interface iGymsRepository {
   findById: (id: string) => Promise<Gym | null>;
+  create: (data: Prisma.GymCreateInput) => Promise<Gym>;
 }
