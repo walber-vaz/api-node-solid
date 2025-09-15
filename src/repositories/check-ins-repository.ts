58531ec35f94e66815a33eb183ae2d21667fd80d
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-14 21:18:03 -0300

    feat: add findByUserIdOnDate method to iCheckInsRepository interface

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,6 +2,7 @@ import type { CheckIn, Prisma } from '../../generated/prisma';
 
 export interface iCheckInsRepository {
   create: (data: Prisma.CheckInUncheckedCreateInput) => Promise<CheckIn>;
+  findByUserIdOnDate: (userId: string, date: Date) => Promise<CheckIn | null>;
   // findById: (id: string) => Promise<CheckIn | null>;
   // findManyByUserId: (userId: string) => Promise<CheckIn[]>;
 }
