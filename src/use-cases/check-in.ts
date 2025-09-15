126b4f8a8c3e57a64f851c7d00c71c396220fcc8
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-14 21:43:07 -0300

    feat: add gym retrieval and user location parameters to CheckInUseCase

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,21 +1,37 @@
+import type { CheckIn } from '@prisma/client';
 import type { iCheckInsRepository } from '@/repositories/check-ins-repository';
-import type { CheckIn } from '../../generated/prisma';
+import type { iGymsRepository } from '@/repositories/gyms-repository';
+import { ResourceNotFoundError } from './erros/resource-not-found-error';
 
 interface iCheckInUseCaseRequest {
   userId: string;
   gymId: string;
+  userLatitude: number;
+  userLongitude: number;
 }
 interface iCheckInUseCaseResponse {
   checkIn: CheckIn;
 }
 
 export class CheckInUseCase {
-  constructor(private checkInsRepository: iCheckInsRepository) {}
+  constructor(
+    private checkInsRepository: iCheckInsRepository,
+    private gymsRepository: iGymsRepository,
+  ) {}
 
   async execute({
     userId,
     gymId,
   }: iCheckInUseCaseRequest): Promise<iCheckInUseCaseResponse> {
+    const gym = await this.gymsRepository.findById(gymId);
+
+    if (!gym) {
+      throw new ResourceNotFoundError();
+    }
+
+    // calculate the distance between user and gym
+    // if the distance is greater than 100 meters, throw an error
+
     const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(
       userId,
       new Date(),
