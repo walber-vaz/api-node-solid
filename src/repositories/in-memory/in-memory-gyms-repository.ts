f4fecfcf9585f50180c256e9ea5408d0acdd4747
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-09-14 22:36:33 -0300

    refactor: reorganize imports and enhance create method in InMemoryGymsRepository

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,4 +1,5 @@
-import type { Gym } from '@prisma/client';
+import { randomUUID } from 'node:crypto';
+import { type Gym, Prisma } from '@prisma/client';
 import type { iGymsRepository } from '../gyms-repository';
 
 export class InMemoryGymsRepository implements iGymsRepository {
@@ -10,6 +11,21 @@ export class InMemoryGymsRepository implements iGymsRepository {
     if (!gym) {
       return null;
     }
+
+    return gym;
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+      created_at: new Date(),
+    };
+    this.items.push(gym);
 
     return gym;
   }
